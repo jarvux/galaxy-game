3ffb97c6ab198522ede0acf7a138ff4bed2cc4a4
JarviZambrano <j.zambrano@uniandes.edu.co>
2023-05-14 17:09:12 -0500

    enemies with movements

--- src/ecs/components/c_enemy_spawner.py
+++ src/ecs/components/c_enemy_spawner.py
@@ -1,31 +1,34 @@
 import pygame
 
+
 class CEnemySpawner:
-    def __init__(self, spawn_events_data:dict,enemies_position_data:dict,initial_position:dict) -> None:
-        self.current_time:float = 0
-        self.spawn_event_data:list[SpawnEventData] = []
-        row_position   = initial_position["x"]
-        colum_position = initial_position["y"]
-        for row in enemies_position_data : 
-            row_position += 25
+     def __init__(self, level_data: dict) -> None:
+        self.current_time: float = 0
+        self.spawn_event_data: list[SpawnEventData] = []
+        row_position = level_data["position"]["x"]
+        colum_position = level_data["position"]["y"]
+        for row in level_data["enemy_spawn_events"]:
+            row_position += 17
             colum_position = 0
-            for column in row : 
+            for column in row:
                 enemy_type = column
-                colum_position += 25
+                colum_position += 15
                 if enemy_type == 0:
                     continue
-                position= pygame.Vector2(colum_position,row_position)
-                self.spawn_event_data.append(SpawnEventData(get_data_enemy(enemy_type,spawn_events_data),position))
+                position = pygame.Vector2(colum_position, row_position)
+                self.spawn_event_data.append(SpawnEventData(get_data_enemy(enemy_type, level_data["enemy_config"]), position))
+
 
 class SpawnEventData:
-    def __init__(self, event_data:dict, position : pygame.Vector2) -> None:
-        self.time:float = event_data["time"]
-        self.enemy_type:str =  event_data["enemy_type"]
-        self.position:pygame.Vector2 = position
+    def __init__(self, event_data: dict, position: pygame.Vector2) -> None:
+        self.time: float = event_data["time"]
+        self.enemy_type: str = event_data["enemy_type"]
+        self.position: pygame.Vector2 = position
         self.triggered = False
 
-def get_data_enemy(enemy_type:int,enemies_data: dict):
-    for data in  enemies_data : 
+
+def get_data_enemy(enemy_type: int, enemies_data: dict):
+    for data in enemies_data:
         if data["enemy_type"] == enemy_type:
             return data
     return None
