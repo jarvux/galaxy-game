65da89b443f3b2c120e864a24e28b6c0a9ce9657
Sebastián Noreña Márquez <90623157+senorena@users.noreply.github.com>
2023-05-16 17:41:14 -0500

    locura extrema

--- src/ecs/systems/s_screen_return.py
+++ src/ecs/systems/s_screen_return.py
@@ -8,11 +8,15 @@ from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_velocity import CVelocity
 from src.ecs.components.tags.c_tag_background import CTagBackground
-
+from src.ecs.components.tags.c_tag_enemy import CTagEnemy
 def system_screen_return_home(world: esper.World, screen: pygame.Surface):
     scr_rect = screen.get_rect()
-    components = world.get_components(CTransform,CSurface,CEnemyHunterState)
-    for enemy_entity, (c_t, c_s, c_st) in components:
+    components = world.get_components(CTransform,CSurface,CEnemyHunterState,CTagEnemy)
+    for enemy_entity, (c_t, c_s, c_st, c_e) in components:
         enemy_rect = CSurface.get_area_relative(c_s.area,c_t.pos)
         if not scr_rect.contains(enemy_rect):
            c_st.state = HunterState.RETURN_HOME
+           c_t.pos.x = 0
+           c_t.pos.y = 0
+        
+
